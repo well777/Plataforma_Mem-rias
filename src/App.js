0efbbbd59a5d14dc54acdbf169bab2cf384d932b
wellington santos <50835319+well777@users.noreply.github.com>
2022-09-19 16:35:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import jpIMG from './assets/brain.svg'
+import './assets/styles.css'
+
+function App() {
+  return (
+    <div className='container'>
+    <div className='container-login'>
+    <div className='wrap-login'>
+    <form className='login-form'>
+
+    
+    
+    <span className='login-form-title'>
+    <img src= {jpIMG} alt='bnn'/>
+    </span>
+    <span className='login-form-title'>Bem Vindo a plataforma Memórias!</span>
+   <div className='wrap-input'>
+   <input className='input' type='email'/>
+   <span className='focus-input' data-placeholder='email'/>
+    </div>
+
+    <div className='wrap-input'>
+    <input className='input' type='password'/>
+    <span className='focus-input' data-placeholder='password'/>
+    </div>
+
+    <div className='container-login-form-btn'>
+    <button className='btn btn-primary'>Login</button>
+    </div>
+<div className='text-center'>
+<span className='txt1'>Não possui conta?</span>
+<a className='txt2' href='#'>Criar conta</a>
+</div>
+    </form>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
